95ffd7e3dc02fd020133a62807ca30ecd423f879
Apeksha Kusabikal <apekshakusabikal@gmail.com>
2026-07-09 18:15:43 +0530

    Add files via upload

---
+++ swapping_nos.py
@@ -0,0 +1,27 @@
+#Swap two numbers
+a = input("enter the value of a =")
+b = input("enter the value of b =")
+print("before swapping")
+print("a="+a)
+print("b="+b)
+print("after swapping")
+temp=a
+a=b
+b=temp
+print("a="+a)
+print("b="+b)
+
+#swap three numbers
+c = input("enter the value of c = ")
+d = input("enter the value of d = ")
+e = input("enter the value of e = ")
+print("before swapping")
+print("c="+c)
+print("d="+d)
+print("e="+e)
+print("after swapping")
+print("c="+e)
+print("d="+c)
+print("e="+d)
+
+
