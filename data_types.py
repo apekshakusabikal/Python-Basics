1af5114d5c50d800c828c54321730a8387957af1
Apeksha Kusabikal <apekshakusabikal@gmail.com>
2026-07-09 18:26:15 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,10 @@
+name = "Apeksha"
+age = 19
+cgpa_1st_year = 9.5
+cgpa_2nd_year = 9.2
+average_cgpa = 1<2
+print(type(name))
+print(type(age))
+print(type(cgpa_1st_year))
+print(type(cgpa_2nd_year))
+print(type(average_cgpa))
