e4c00cb36e48969fc6496542db82dc2de5aee02e
Apeksha Kusabikal <apekshakusabikal@gmail.com>
2026-07-10 22:16:59 +0530

    Add files via upload

---
+++ f_string.py
@@ -0,0 +1,12 @@
+#1
+name = 'Apeksha'
+age=19
+height=175
+print(f"My name is {name}. I am {age} years old. My height is {height} centimeters")
+
+#college and branch
+college='Kle Technological University'
+branch='Electronics Engineering'
+year='1'
+print(f"I study at {college} Belgaum. My branch is {branch} and pursuing {year}st year")
+
