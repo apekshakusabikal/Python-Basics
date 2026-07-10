bf8067def869e95bd108cdb9e6a96bf5ede9d94a
Apeksha Kusabikal <apekshakusabikal@gmail.com>
2026-07-10 21:01:57 +0530

    Add files via upload

---
+++ round_function.py
@@ -0,0 +1,21 @@
+#1
+print(round(3.14159))
+print(round(7.89,1))
+print(round(12.3456, 2))
+print(round(98.76543,3))
+print(round(10/3,2))
+
+#average of 3 numbers
+a=12
+b=3
+c=6
+average=(a+b+c)/3
+print("average=",average)
+print(round(average,2))
+
+#area of circle
+radius=3
+area=3.14*radius*radius
+print("area=",area)
+print(round(area,2))
+
