138ffa68f52efe0951e8547395d9152c9de08981
Apeksha Kusabikal <apekshakusabikal@gmail.com>
2026-07-10 13:48:55 +0530

    Add files via upload

---
+++ biwise_operators.py
@@ -0,0 +1,77 @@
+#addition
+a=10
+a+=2
+print(a)
+
+#substraction
+a=10
+a-=2
+print(a)
+
+#multiplication
+a=5
+a*=4
+print(a)
+
+#division
+a=100
+a/=5
+print(a)
+
+#remainder
+a=25
+a%=4
+print(a)
+
+#floor division
+a=12
+a//=4
+print(a)
+
+#power
+a=3
+a**=2
+print(a)
+
+#bitwise AND
+a=12
+a&=4
+print(a)
+
+#bitwise OR
+a=12
+a|=4
+print(a)
+
+#bitwise XOR
+a=16
+a^=4
+print(a)
+
+#left shift
+a=8
+a<<=2
+print(a)
+
+#right shift
+a=4
+a>>=2
+print(a)
+
+#take a number as input and add 10 to it
+num_1=int(input("enter a number: "))
+print(num_1)
+num_1+=10
+print(num_1)
+
+#take a number as input and multiply it by 2
+num_1=int(input("enter a number: "))
+print(num_1)
+num_1*=2
+print(num_1)
+
+#taking a number as input and square using **=
+num_1=int(input("enter the number: "))
+print(num_1)
+num_1**=3
+print(num_1)
