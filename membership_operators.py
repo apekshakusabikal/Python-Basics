4ddb10f6cdf2da47658092844f6876a965f71157
Apeksha Kusabikal <apekshakusabikal@gmail.com>
2026-07-10 18:02:31 +0530

    Add files via upload

---
+++ membership_operators.py
@@ -0,0 +1,16 @@
+str='Apple'
+print('A' in str)
+print('a' in str)
+
+list=[1,2,3,4,5,6,7,8]
+print(10 in list)
+print(1,2,3 in list)
+print(1,2,3,4,5,6,7,8 in list)
+print(1,2,3,4,5,6,7,8,9 in list)
+print(1 not in list)
+
+str='icecream'
+print('i' in str)
+print('c' not in str)
+print('ice cream' in str)
+print('icecream' in str)
