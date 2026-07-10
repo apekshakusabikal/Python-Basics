d5242b18b0015498913f131f43b7712d5c13af6f
Apeksha Kusabikal <apekshakusabikal@gmail.com>
2026-07-10 11:27:59 +0530

    Add files via upload

---
+++ type_casting.py
@@ -0,0 +1,127 @@
+#convert integer 15 into a float
+num=15
+new_num=float(num)
+print(new_num)
+print(type(new_num))
+
+#convert the float 45.9 into an integer
+num=45.9
+new_num=int(num)
+print(new_num)
+print(type(new_num))
+
+#convert the integer 100 into a string
+num=100
+new_num=str(num)
+print(new_num)
+print(type(new_num))
+
+#convert the string "250" into an integer
+num="250"
+new_num=int(num)
+print(new_num)
+print(type(new_num))
+
+#convert the string "3.1435" into a float
+num="3.1435"
+new_num=float(num)
+print(new_num)
+print(type(new_num))
+
+#convert the integer 1 into a boolen
+num=1
+new_num=bool(num)
+print(new_num)
+print(type(new_num))
+
+#convert the integer 0 into a boolen
+num=0
+new_num=bool(num)
+print(new_num)
+print(type(new_num))
+
+#convert the boolen true into an integer
+num=True
+new_num=int(num)
+print(new_num)
+print(type(new_num))
+
+#convert the boolen false into an integer
+num=False
+new_num=int(num)
+print(new_num)
+print(type(new_num))
+
+#convert the float 99.99 into a string
+num=99.99
+new_num=str(num)
+print(new_num)
+print(type(new_num))
+
+#convert the string "500" into a float
+num="500"
+new_num=float(num)
+print(new_num)
+print(type(new_num))
+
+#convert the integer 25 into a boolen
+num=25
+new_num=bool(num)
+print(new_num)
+print(type(new_num))
+
+#convert the string "0" into an integer
+num="0"
+new_num=int(num)
+print(new_num)
+print(type(new_num))
+
+#convert the string "0" into an integer
+num="0"
+new_num=int(num)
+print(new_num)
+print(type(new_num))
+
+#convert the float 10.75 into a string
+num=10.75
+new_num=str(num)
+print(new_num)
+print(type(new_num))
+
+#convert the string "false" into a boolen
+num="false"
+new_num = bool(num)
+print(new_num)
+print(type(new_num))
+
+#convert the integer -5 into a boolen
+num=-5
+new_num=bool(num)
+print(new_num)
+print(type(new_num))
+
+#convert the float 0.0 into a boolen
+num=0.0
+new_num=bool(num)
+print(new_num)
+print(type(new_num))
+
+#taking a number as input and convert it into an float
+num = input("enter a number ")
+new_num=float(num)
+print(new_num)
+print(type(new_num))
+
+#taking 2 numbers as input and convert it into an float and add them
+num_1 = input("enter a first number ")
+num_2 = input("enter a second number")
+new_num_1=float(num_1)
+print(new_num_1)
+print(type(new_num_1))
+
+new_num_2=float(num_2)
+print(new_num_2)
+print(type(new_num_2))
+print("after adding")
+print(new_num_1+new_num_2)
+
