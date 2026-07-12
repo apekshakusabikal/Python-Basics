9bee0f9728e20e14459b8e32856ae59966003f9d
Apeksha Kusabikal <apekshakusabikal@gmail.com>
2026-07-12 11:46:20 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,59 @@
+#check whether the number is positive or negative
+num=int(input("enter a number: "))
+if(num>0):
+    print("*the number is positive")
+else:
+    print("*the number is negative")
+    
+#check the number is even or odd
+num=int(input("enter the number: "))
+if(num %2==0):
+    print("*the number is even")
+else:
+    print("*the number is odd")
+    
+#check whether the person is eligible to vote
+age=int(input("enter the age: "))
+if(age>=18):
+    print("*the person is eligible for voting")
+else:
+    print("*the person is not eligible for voting ")
+    
+#check whether the number is divisible by 5
+num=int(input("enter the number: "))
+if(num%5==0):
+    print("*the number is divisible by 5")
+else:
+    print("*the number is not divisible by 5")
+    
+#check whether the number is divisible by 2 and 3
+num=int(input("enter the number: "))
+if(num%2==0 and num%3==0):
+    print("*the number is divisible by both 2 and 3")
+else:
+    print("*the number is not divisible by both 2 and 3")
+    
+#check whether the number is greater than 100
+num=int(input("enter the number: "))
+if(num>100):
+    print("*the number is greater than 100")
+else:
+    print("*the number is smaller than 100")
+    
+#check whether the number is zero or non zero
+num=int(input("enter the number: "))
+if(num==0):
+    print("the number is zero")
+else:
+    print("the number is non zero")
+    
+#compare two numbers and print greater number
+num_1=int(input("enter the first number: "))
+num_2=int(input("enter the seconf number: "))
+if(num_1>num_2):
+    print("first number is greater")
+else:
+    print("second number is greater")
+    
+
+
